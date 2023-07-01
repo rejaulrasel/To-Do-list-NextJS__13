1b2c3ce04b93383980c47927c4e7839abfeabfa6
rejaul rasel <rejaulhasanrasel28@gmail.com>
2023-07-01 10:59:33 +0600

    added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1 className="text-5xl">Hello</h1>
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className='text-5xl'>ToDos</h1>
+        <Link className="text-5xl border border-slate-100 tex p-3 hover:bg-slate-700 focus-within:bg-slate-700 outline:none" href='/new'>New</Link>
+      </header>
+    </>
+  )
 }
